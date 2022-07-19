aa5650dec3daaa0bdba4292f820b189df1021019
Nstkrnv <kornevaann99@gmail.com>
2022-07-19 22:49:18 +0300

    Delete OuterApp.java

--- src/main/java/com/FML/OuterApp.java
+++
@@ -1,9 +0,0 @@
-//package com.FML;
-//
-//import static javafx.application.Application.launch;
-//
-//public class OuterApp{
-//    public static void main(String[] args) {
-//        com.FML.App.run();
-//    }
-//}
